aa200541157f0a2d63b6756c8e947d3a6cc99387
Tom Wardill <tomwardill@NuisanceValue.(none)>
2009-12-25 22:11:58 +0000

    First pass at models

--- web/app/models.py
+++ web/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+meter_types = (('1', 'Mains'),
+               ('2', 'Appliance'),)
+
+class Reading(models.Model):
+    """
+    A Reading from the CC meter
+    """
+    time = models.DateTimeField()
+    temperature = models.DecimalField()
+
+class SensorReading(models.Model):
+    """
+    A value from a sensor, related to a reading taken at a particular time
+    """
+    meter_id = models.CharField(max_length = 5)
+    meter_type = models.IntegerField()
+    wattage = models.DecimalField()
+    reading = models.ForeignKey(Reading)    
