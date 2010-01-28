f0326f0c2e2e35f7ef5cc43ce0aed431412d0c50
Thomas Wardill <tomwardill@SweetAndFullOfGrace.local>
2010-01-28 20:12:56 +0000

    Updated createdb script for using postgresql and psycopg2

--- daemon/createdb.py
+++ daemon/createdb.py
@@ -1,6 +1,10 @@
-import sqlite3
+import psycopg2
 
-conn = sqlite3.connect('daemon-data.db')
+conn = psycopg2.connect("dbname='powercost' user='powercost' host='localhost' password='costpower'")
+cur = conn.cursor()
 
-conn.execute(""" create table powercost(date text, temperature numeric, sensor_id integer, meter_type integer, ch1 numeric, ch2 numeric, ch3 numeric  )  """)
-
+try:
+    cur.execute(""" create table powercost(date text, temperature numeric, sensor_id integer, meter_type integer, ch1 numeric, ch2 numeric, ch3 numeric  )  """)
+    conn.commit()
+except Exception, err:
+    print err
